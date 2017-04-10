3e3f81ce71f6948f3826bcda33591f7767a0af86
Benjamin Thomas <benjaminthomas1999@outlook.com>
2017-04-10 18:38:11 +0100

    Multifiles and Collision Detection

---
+++ entity.py
@@ -0,0 +1,182 @@
+import pygame
+import constant
+from datetime import datetime
+
+class Player(object):
+	def __init__(self, startPosition, size):		
+		self.position = startPosition
+		self.velocity = [0, 0]
+		self.size = size
+		
+		self.moving = {"left":False, "right":False, "up":False, "down":False}
+		self.growing = False
+		self.shrinking = False
+		self.minSize = 5
+		self.maxSize = 60
+		self.sens = 0.05
+		self.terminal = 5
+		self.friction = 0.02
+		self.maxHealth = 790
+		self.health = self.maxHealth
+	
+		
+	def gravityAcc(self):
+		return constant.GRAVITY*self.size**2
+		
+	def update(self):		
+		if self.moving["left"]:
+			self.velocity[0] -= self.sens
+		if self.moving["right"]:
+			self.velocity[0] += self.sens
+		if self.moving["up"]:
+			self.velocity[1] -= self.sens
+		if self.moving["down"]:
+			self.velocity[1] += self.sens
+		
+		#friction
+		if abs(self.velocity[0]) > 0:
+			self.velocity[0] -= self.friction*(abs(self.velocity[0])/self.velocity[0])
+		if abs(self.velocity[1]) > 0:
+			self.velocity[1] -= self.friction*(abs(self.velocity[1])/self.velocity[1])
+		
+		#terminal velocity
+		if abs(self.velocity[0]) > self.terminal:
+			self.velocity[0] = self.terminal*(abs(self.velocity[0])/self.velocity[0])
+		if self.velocity[1] < -self.terminal:#no terminal vel going down
+			self.velocity[1] = -self.terminal
+			
+		#acceleration due to gravity
+		self.velocity[1] += self.gravityAcc()
+		
+		#update position
+		self.position[0] += self.velocity[0]
+		self.position[1] += self.velocity[1]
+		
+		
+		#stops jittering
+		if self.velocity[0] > -0.001 and self.velocity[0] < 0.001:
+			self.velocity[0] = 0
+		if self.velocity[1] > -0.001 and self.velocity[1] < 0.001:
+			self.velocity[1] = 0
+		
+		#growing and shrinking
+		if self.growing and self.size < self.maxSize:
+			self.size += 0.5
+			self.position[0] -= 0.25
+			self.position[1] -= 0.25
+			percentHealth = self.health/self.maxHealth
+			self.maxHealth += 6
+			self.health = self.maxHealth*percentHealth
+		
+		if self.shrinking and self.size > self.minSize:
+			self.size -= 0.5
+			self.position[0] += 0.25
+			self.position[1] += 0.25
+			percentHealth = self.health/self.maxHealth
+			self.maxHealth -= 6
+			self.health = self.maxHealth*percentHealth
+		
+		#screen limits
+		if self.position[1]+self.size > constant.HEIGHT: 
+			self.position[1] = constant.HEIGHT - self.size
+			self.velocity[1] = 0
+			self.moving["down"] = False
+		elif self.position[1] < 20:
+			self.position[1] = 20
+			self.velocity[1] = 0
+			self.velocity[1] = self.gravityAcc()
+			self.moving["up"] = False
+		
+		if self.position[0]+self.size > constant.WIDTH:
+			self.position[0] = constant.WIDTH-self.size
+			self.velocity[0] = 0
+			self.moving["right"] = False
+		elif self.position[0] < 0:
+			self.position[0] = 0
+			self.velocity[0] = 0
+			self.moving["left"] = False
+		
+
+		
+	def damage(self, amount):
+		if self.health-amount <= 0:
+			print("Game over")
+			self.health = 0
+		else:
+			self.health -= amount
+	
+	def heal(self, amount):
+		self.health += amount
+		if self.health > self.maxHealth:
+			self.health = self.maxHealth
+			
+	
+	def draw(self, windowSurface):
+		pygame.draw.rect(windowSurface, constant.BLUE, (self.position[0], self.position[1], self.size, self.size))
+		
+		pygame.draw.rect(windowSurface, constant.BLACK, (5, 5, self.maxHealth, 15))
+		pygame.draw.rect(windowSurface, constant.RED, (5, 5, self.health, 15))
+	
+	
+class Spike(object):
+	width = 20
+	height = 20
+	
+	def __init__(self, position):
+		self.position = position
+	
+	def update(self, player):
+		if player.position[0]+player.size > self.position[0] and player.position[0] < self.position[0]+self.width:
+			if player.position[1]+player.size > self.position[1] and player.position[1] < self.position[1]+self.height:
+				player.damage(2)
+				
+				if player.position[0]+player.size < self.position[0]+self.width/2:
+					player.position[0] = self.position[0]-player.size
+					player.velocity[0] = 0
+				if player.position[0] > self.position[0]+self.width/2:
+					player.position[0] = self.position[0]+self.width
+					player.velocity[0] = 0
+					
+				elif player.position[1]+player.size < self.position[1]+self.height/2:
+					player.position[1] = self.position[1]-player.size
+					player.velocity[1] = 0
+	
+	def draw(self, windowSurface):
+		pygame.draw.polygon(windowSurface, constant.BLACK, [(self.position[0]+self.width/2, self.position[1]), (self.position[0], self.position[1]+self.height), (self.position[0]+self.width, self.position[1]+self.height)])
+	
+class Wall(object):
+	def __init__(self, position, width, height):
+		self.position = position
+		self.width = width
+		self.height = height
+	
+	
+		
+	
+	def update(self, player):
+		if player.position[1]+player.size > self.position[1] and player.position[1] < self.position[1]+self.height:
+			if player.position[0] < self.position[0]+self.width and player.position[0]+player.size > self.position[0]:
+				
+				if player.position[1]+player.size < self.position[1]+10:
+					player.position[1] = self.position[0]-player.size
+					player.velocity[1] = 0
+					player.moving["down"] = False
+				
+				elif player.position[0] > self.position[0]+self.width-10:
+					player.position[0] = self.position[0]+self.width
+					player.velocity[0] = 0
+					player.moving["left"] = False
+				
+				elif player.position[0]+player.size < self.position[0]+10:
+					player.position[0] = self.position[0]-player.size
+					player.velocity[0] = 0
+					player.moving["right"] = False
+					
+				elif player.position[1] > self.position[1]+self.height-10:
+					player.position[1] = self.position[1]+self.height
+					player.velocity[1] = player.gravityAcc()
+					player.moving["up"] = False
+				
+	
+	def draw(self, windowSurface):
+		pygame.draw.rect(windowSurface, constant.BLACK, (self.position[0], self.position[1], self.width, self.height))
--- main.py
+++ main.py
@@ -1,134 +1,19 @@
 import pygame, sys, time, random
 from pygame.locals import *
-pygame.init()
-HEIGHT = 500
-WIDTH = 800
-GRAVITY = 0.0001
+import entity
+import constant
 
-windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.init()
+
+windowSurface = pygame.display.set_mode((constant.WIDTH, constant.HEIGHT), 0, 32)
 pygame.display.set_caption('Hop Hop Hop')
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
-class Player(object):
-	def __init__(self, startPosition, size):
-		self.position = startPosition
-		self.velocity = [0, 0]
-		self.size = size
-		
-		self.moving = {"left":False, "right":False, "up":False, "down":False}
-		self.growing = False
-		self.shrinking = False
-		self.minSize = 5
-		self.maxSize = 60
-		self.sens = 0.05
-		self.terminal = 5
-		self.friction = 0.02
-		self.maxHealth = 790
-		self.health = self.maxHealth
-	
-	def update(self):
-		if self.moving["left"]:
-			self.velocity[0] -= self.sens
-		if self.moving["right"]:
-			self.velocity[0] += self.sens
-		if self.moving["up"]:
-			self.velocity[1] -= self.sens
-		if self.moving["down"]:
-			self.velocity[1] += self.sens
-		
-		#friction
-		if abs(self.velocity[0]) > 0:
-			self.velocity[0] -= self.friction*(abs(self.velocity[0])/self.velocity[0])
-		if abs(self.velocity[1]) > 0:
-			self.velocity[1] -= self.friction*(abs(self.velocity[1])/self.velocity[1])
-		
-		#terminal velocity
-		if abs(self.velocity[0]) > self.terminal:
-			self.velocity[0] = self.terminal*(abs(self.velocity[0])/self.velocity[0])
-		if self.velocity[1] < -self.terminal:#no terminal vel going down
-			self.velocity[1] = -self.terminal
-			
-		#acceleration due to gravity
-		self.velocity[1] += GRAVITY*self.size**2
-		
-		#update position
-		self.position[0] += self.velocity[0]
-		self.position[1] += self.velocity[1]
-		
-		#stops jittering
-		if self.velocity[0] > -0.001 and self.velocity[0] < 0.001:
-			self.velocity[0] = 0
-		if self.velocity[1] > -0.001 and self.velocity[1] < 0.001:
-			self.velocity[1] = 0
-		
-		
-		if self.growing and self.size < self.maxSize:
-			self.size += 0.5
-			self.position[0] -= 0.25
-			self.position[1] -= 0.25
-			percentHealth = self.health/self.maxHealth
-			self.maxHealth += 6
-			self.health = self.maxHealth*percentHealth
-		
-		if self.shrinking and self.size > self.minSize:
-			self.size -= 0.5
-			self.position[0] += 0.25
-			self.position[1] += 0.25
-			percentHealth = self.health/self.maxHealth
-			self.maxHealth -= 6
-			self.health = self.maxHealth*percentHealth
-		
-		#screen limits and wrapping
-		if self.position[1]+self.size > HEIGHT: 
-			self.position[1] = HEIGHT - self.size
-			self.velocity[1] = 0
-		elif self.position[1] < 20:
-			self.position[1] = 20
-			self.velocity[1] = 0
-		if self.position[0] > WIDTH:
-			self.position[0] = -self.size+1
-		elif self.position[0] < -self.size:
-			self.position[0] = WIDTH-1
-		
-		
-		self.draw()
-	def damage(self, amount):
-		if self.health-amount <= 0:
-			print("Game over")
-		else:
-			self.health -= amount
-	
-	def heal(self, amount):
-		self.health += amount
-		if self.health > self.maxHealth:
-			self.health = self.maxHealth
-	
-	def draw(self):
-		pygame.draw.rect(windowSurface, BLUE, (self.position[0], self.position[1], self.size, self.size))
-		
-		pygame.draw.rect(windowSurface, BLACK, (5, 5, self.maxHealth, 15))
-		pygame.draw.rect(windowSurface, RED, (5, 5, self.health, 15))
-	
-	
-class Spike(object):
-	def __init__(self, ):
-		self.position = [-30, random.randint(50, HEIGHT-40)]
-		if spawnOnRight:
-			self.position[0] = WIDTH+40
-	
-	def draw(self):
-		None
-	def update():
-		None
-			
-
-
-player = Player([30, HEIGHT-40], 60)
+player = entity.Player([500, 0], 60)
+spikes = []
+for i in range(10):
+	spikes.append(entity.Spike([200+entity.Spike.width*i, constant.HEIGHT-entity.Spike.height]))
+wall = entity.Wall([200, 200], 100, 100)
 
 while True:
 	for event in pygame.event.get():
@@ -149,10 +34,8 @@ while True:
 				player.growing = True
 			elif event.key == pygame.K_LSHIFT:
 				player.shrinking = True
-			elif event.key == pygame.K_h:
-				player.damage(10)
 			elif event.key == pygame.K_j:
-				player.heal(10)
+				player.heal(100)
 				
 		elif event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT or event.key == pygame.K_a:
@@ -168,8 +51,16 @@ while True:
 			elif event.key == pygame.K_LSHIFT:
 				player.shrinking = False
 	
-	windowSurface.fill(WHITE)
+	windowSurface.fill(constant.WHITE)
+	
+	for spike in spikes:
+		spike.update(player)
+		spike.draw(windowSurface)
+	
 	
 	player.update()
+	player.draw(windowSurface)
+	wall.update(player)
+	wall.draw(windowSurface)
 	pygame.display.update()
 	time.sleep(0.002)
